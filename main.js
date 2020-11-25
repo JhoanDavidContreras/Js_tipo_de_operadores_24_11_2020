ec1453892e5abc380ab32dfcc897cbf4d8d45c13
unknown <Jhoandavidcontreras14@gmail.com>
2020-11-24 19:25:38 -0500

    Js_tipo_de_operadores 24-11-2020

---
+++ main.js
@@ -0,0 +1,94 @@
+/*console.log("soy el archivo main.js conectado exitosamente");
+alert("soy el archivo main.js conectado exitosamente")
+
+
+let nombre_usuario; 
+
+nombre_usuario=prompt("ingrese su usuario");
+
+console.log("El nombre del usuario es",nombre_usuario)*/
+
+
+
+
+
+/*let numero_1 = 5;
+let numero_2 = 10;
+
+
+console.log("contenido de la variable 1 nombre de la variable numero_1:", numero_1 );
+console.log("contenido de la variable 2 nombre de la variable numero_2:", numero_2 );
+
+
+numero_1 = numero_2;
+console.log(numero_1);*/
+
+
+
+
+
+
+
+
+
+
+
+
+let numero_1 = 1;
+let numero_2 = 2;
+let resultado;
+
+
+numero_1 +=numero_2;
+
+
+console.log(numero_1);
+
+
+
+
+
+
+
+ numero_1 = 2;
+ numero_2 = 25;
+
+numero_1 *=numero_2;
+
+
+
+console.log(numero_1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let numero_1 = 2;
+let numero_2 = 25;
+
+numero_2
+
+
+
+
+
+
+
+
+
+
+
+
